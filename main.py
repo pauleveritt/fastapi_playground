39fdf086874a9db96d551c9ec6c77ef8d1055ce9
Paul Everitt <pauleveritt@me.com>
2025-10-19 17:43:39 +0000

    Updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI in Codespaces!!!!"}
+    return {"message": "Hello from FastAPI in Codespaces!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
